ca7b4646d88668a4da3c676150c01596b9ca79db
Goubt <72228959+Goubt@users.noreply.github.com>
2024-04-23 12:05:41 +0100

    Creature generating
    No sin wave to frequency , cannot move creature

--- GE2-Test-Godot/behaviors/creature_generator.gd
+++ GE2-Test-Godot/behaviors/creature_generator.gd
@@ -9,20 +9,41 @@ extends Node3D
 
 
 func _process(delta):
+	var yAxis = 0
 	for i in range(length):
-		var angle = sin(i * frequency + start_angle)
-		print(angle)
+		var angle = sin(i/PI)
 		var size = remap(angle, 0, 1, base_size, base_size * multiplier)
 		var box_size = Vector3(size, size, size)
-		var position = Vector3(0-size/2, 0-size/2,i *size)
+		var position = Vector3(0-size/2, 0-size/2,yAxis)
 		DebugDraw3D.draw_box(position, Quaternion(), box_size, Color.BLUE)
+		yAxis = yAxis + size
 	pass
 
 func _ready():
-	if not Engine.is_editor_hint():
+	if not Engine.is_editor_hint():		
 		pass
-	
-	
+	if not Engine.is_editor_hint():
+		var head_scene = preload("res://behaviors/head.tscn")
+		var body_scene = preload("res://behaviors/body.tscn")
+		var yAxis = 0
 
+		for i in range(length):
+			var segment
+			var angle = sin(i/PI)
+			var size = remap(angle, 0, 1, base_size, base_size * multiplier)
+			var box_size = Vector3(size, size, size)
+			yAxis = yAxis + size
+			var position = Vector3(0, 0,yAxis)
+			
+			
+			if i == 0:
+				segment = head_scene.instantiate()
+			else:
+				segment = body_scene.instantiate()
+				
+			segment.size = Vector3(size, size, size)
+			segment.transform.origin = position
+			
+			add_child(segment)
 
 
